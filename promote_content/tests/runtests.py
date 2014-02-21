e99745aae59ed7dff4bee626aab8ec52eea1956b
Tosh Lyons <tosh.lyons@gmail.com>
2014-02-21 12:40:55 -0600

    custom test runner
    allows tests to be run outside of a project

---
+++ promote_content/tests/runtests.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+
+from django.conf import settings
+from django.test.simple import DjangoTestSuiteRunner
+
+test_runner = DjangoTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(['promote_content', ])
+if failures:
+    sys.exit(failures)
