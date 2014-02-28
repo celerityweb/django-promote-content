40b274e688c449052917d5578abd72ca1102057e
Tosh Lyons <tosh.lyons@gmail.com>
2014-02-27 19:59:04 -0600

    basic curation admin

--- promote_content/admin.py
+++ promote_content/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Curation
 
-admin.site.register(Curation)
+from genericadmin.admin import GenericAdminModelAdmin
+
+
+class CurateAdmin(GenericAdminModelAdmin):
+    generic_fk_fields = [{
+        'ct_field': 'context_type',
+        'fk_field': 'context_id',
+    },
+    {
+        'ct_field': 'content_type',
+        'fk_field': 'content_id',
+    }]
+admin.site.register(Curation, CurateAdmin)
