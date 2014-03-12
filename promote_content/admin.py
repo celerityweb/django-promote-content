a0700b9acbeffa9d288c9b96c34aa702c6f0d678
Tosh Lyons <tosh.lyons@gmail.com>
2014-03-11 19:59:38 -0500

    working out how to dynamically set content_type_whitelist

--- promote_content/admin.py
+++ promote_content/admin.py
@@ -11,6 +11,19 @@ class CurateAutoContextForm(forms.ModelForm):
         model = Curation
         exclude = ['context_id', 'context_type', ]
 
+# imports for genericadmin_js_init
+from django.contrib.contenttypes.models import ContentType
+try:
+    from django.utils.encoding import force_text
+except ImportError:
+    from django.utils.encoding import force_unicode as force_text
+from django.contrib.admin.widgets import url_params_from_lookup_dict
+try:
+    from django.contrib.admin.views.main import IS_POPUP_VAR
+except ImportError:
+    from django.contrib.admin.options import IS_POPUP_VAR
+import json
+from django.http import HttpResponse
 
 class CurateAdmin(GenericAdminModelAdmin):
     generic_fk_fields = [{
@@ -30,6 +43,9 @@ class CurateAdmin(GenericAdminModelAdmin):
             request.session['_for_context'] = True
             request.session['context_id'] = request.GET['context_id']
             request.session['context_type'] = request.GET['context_type']
+        if request.GET.get('cts', None):
+            request.session['content_type_whitelist'] = (request.GET['cts'])
+
         return super(CurateAdmin, self).add_view(request, form_url, extra_context)
 
     def save_model(self, request, obj, form, change):
@@ -41,6 +57,7 @@ class CurateAdmin(GenericAdminModelAdmin):
             request.session.pop('_for_context')
             request.session.pop('context_type')
             request.session.pop('context_id')
+            request.session.pop('content_type_whitelist')
 
     def get_form(self, request, obj=None, **kwargs):
         if request.session.get('_for_context', None):
@@ -48,6 +65,35 @@ class CurateAdmin(GenericAdminModelAdmin):
         else:
             form = super(CurateAdmin, self).get_form(request, obj, **kwargs)
         return form
+
+    # hackey fix for overridding/setting content_type_whitelist per request
+    def genericadmin_js_init(self, request):
+        if request.method == 'GET':
+            obj_dict = {}
+            for c in ContentType.objects.all():
+                val = force_text('%s/%s' % (c.app_label, c.model))
+                params = self.content_type_lookups.get('%s.%s' % (c.app_label, c.model), {})
+                params = url_params_from_lookup_dict(params)
+                whitelist = None
+                if request.session.get('content_type_whitelist', None):
+                    whitelist = request.session['content_type_whitelist']
+                elif self.content_type_whitelist:
+                    whitelist = self.content_type_whitelist
+
+                if whitelist:
+                    if val in whitelist:
+                        obj_dict[c.id] = (val, params)
+                elif val not in self.content_type_blacklist:
+                    obj_dict[c.id] = (val, params)
+
+            data = {
+                'url_array': obj_dict,
+                'fields': self.get_generic_field_list(request),
+                'popup_var': IS_POPUP_VAR,
+            }
+            resp = json.dumps(data, ensure_ascii=False)
+            return HttpResponse(resp, mimetype='application/json')
+        return HttpResponseNotAllowed(['GET'])
 
 admin.site.register(Curation, CurateAdmin)
 
