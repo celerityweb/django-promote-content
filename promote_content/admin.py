b37ffffb9d1d0317d521fa0be68de70e2902b5e1
Tosh Lyons <tosh.lyons@gmail.com>
2014-03-12 20:21:22 -0500

    in-progress take at bulk managing curations

--- promote_content/admin.py
+++ promote_content/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
+from django.conf.urls import patterns, url
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+from django.utils.translation import ungettext
+from django.utils.encoding import force_unicode
 
 from .models import Curation
 
 from genericadmin.admin import GenericAdminModelAdmin, GenericTabularInline, GenericStackedInline
 
-# TODO move form to forms.py
 from django import forms
+from django.forms.formsets import formset_factory
+from django.forms.models import modelformset_factory, inlineformset_factory
+
 class CurateAutoContextForm(forms.ModelForm):
     class Meta:
         model = Curation
         exclude = ['context_id', 'context_type', ]
+
 
 # imports for genericadmin_js_init
 from django.contrib.contenttypes.models import ContentType
@@ -25,6 +34,7 @@ except ImportError:
 import json
 from django.http import HttpResponse
 
+
 class CurateAdmin(GenericAdminModelAdmin):
     generic_fk_fields = [{
         'ct_field': 'context_type',
@@ -34,6 +44,12 @@ class CurateAdmin(GenericAdminModelAdmin):
         'ct_field': 'content_type',
         'fk_field': 'content_id',
     }]
+
+    def get_urls(self):
+        custom_urls = patterns('',
+                               url(r'^multi-context/$', self.admin_site.admin_view(self.multi_context), name='multi_context'),
+        )
+        return custom_urls + super(CurateAdmin, self).get_urls()
 
     def add_view(self, request, form_url='', extra_context=None):
         # Calling add_view with a querystring of the form ?context_type=34&context_id=152&_popup=1
@@ -47,6 +63,63 @@ class CurateAdmin(GenericAdminModelAdmin):
             request.session['content_type_whitelist'] = (request.GET['cts'])
 
         return super(CurateAdmin, self).add_view(request, form_url, extra_context)
+
+    def multi_context(self, request, form_url='', extra_context=None):
+        # TODO handle popup param
+        if request.GET.get('context_id', None) and request.GET.get('context_type', None):
+            context_id = request.session['context_id'] = request.GET['context_id']
+            context_type = request.session['context_type'] = request.GET['context_type']
+        else:
+            context_id = request.session.get('context_id', None)
+            context_type = request.session.get('context_type', None)
+
+        # if these are not set here we should probably raise an error
+        if context_id and context_type:
+            context_content_type = ContentType.objects.get_for_id(context_type)
+            context_object = context_content_type.get_object_for_this_type(pk=context_id)
+
+        if request.GET.get('cts', None):
+            request.session['content_type_whitelist'] = (request.GET['cts'])
+
+        if context_id and context_type:
+            CurationFormSet = modelformset_factory(Curation, extra=3, can_delete=True, form=CurateAutoContextForm)
+        else:
+            CurationFormSet = modelformset_factory(Curation, extra=3, can_delete=True)
+
+        if request.method == "POST":
+            print request.POST
+            formset = CurationFormSet(request.POST, request.FILES)
+            if formset.is_valid():
+                instances = formset.save(commit=False)
+                for obj in instances:
+                    obj.context_object = context_object
+                    obj.save()
+                request.session.pop('context_id')
+                request.session.pop('context_type')
+                return self.response_add(request, context_object)
+        else:
+            formset = CurationFormSet(
+                queryset=Curation.objects.filter(
+                    context_type=context_type, context_id=context_id
+                ).order_by('-weight', 'content_type'))
+
+        context = {
+            'opts': self.model._meta,
+
+            'formset':formset,
+            'context_type': context_type,
+            'context_id': context_id,
+            'context_content_type': context_content_type,
+            'context_object': context_object,
+        }
+
+
+        return render(
+            request,
+            "admin/curation/multi_context.html",
+            context,
+        )
+
 
     def save_model(self, request, obj, form, change):
         if type(form) == CurateAutoContextForm:
