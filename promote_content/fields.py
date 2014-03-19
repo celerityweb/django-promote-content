5e57eff1d6a72293c43c15fe0d9a07de8df6049b
Tosh Lyons <tosh.lyons@gmail.com>
2014-03-18 19:14:20 -0500

    add curation form field and widget
    handle pop up

--- promote_content/fields.py
+++ promote_content/fields.py
@@ -1,5 +1,9 @@
+from django import forms
+
 from django.contrib.contenttypes.generic import GenericRelation
 from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
 
 from .models import Curation
 
@@ -13,3 +17,28 @@ class CurationField(GenericRelation):
         kwargs['object_id_field'] = kwargs.pop("object_id_field", "content_id")
         kwargs['content_type_field'] = kwargs.pop("content_type_field", "content_type")
         super(CurationField, self).__init__(to, **kwargs)
+
+
+
+class CurationsWidget(forms.Widget):
+    """
+    Form widget for rendering Curations pop up link
+    """
+    # def __init__(self, attrs=None,
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        return mark_safe(u'<a href="javascript:window.open(\'%s\', \'manage_curations\', \'height=500,width=800,resizable=yes,scrollbars=yes\')">Manage Curations</a>' % reverse('admin:multi_context'))
+
+
+class Curations(forms.Field):
+    widget = CurationsWidget
+    validators = []
+    initial = ''
+    required = False
+
+    def __init__(self, label=None, help_text='', widget=None):
+        self.label = label
+        self.help_text = help_text
+        widget = widget or self.widget
+        self.widget = widget()
